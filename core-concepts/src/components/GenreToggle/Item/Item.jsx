f04af77895b0ed7e66afb7fd5e126f0b52df5fdc
Tamas Kovacs <kovacs.tamas939@gmail.com>
2021-09-13 22:43:47 +0200

    Now return is only one line in the Item component

--- core-concepts/src/components/GenreToggle/Item/Item.jsx
+++ core-concepts/src/components/GenreToggle/Item/Item.jsx
@@ -5,9 +5,7 @@ import './Item.css'
 const Item = ({ onClick, activeFilter, itemName }) => {
     const liClassName = activeFilter === itemName ? 'Active-filter' : 'Not-active-filter'
 
-    return (
-        <li onClick={onClick} className={liClassName}>{itemName}</li>
-    )
+    return <li onClick={onClick} className={liClassName}>{itemName}</li>
 }
 
 export default Item
